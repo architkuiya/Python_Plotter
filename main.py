07f3dff983b78380093467299579ba1619ace49d
archit <archit@localhost.localdomain>
2021-10-28 15:57:50 +0530

    second window button object names corrected

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
     QPushButton, QVBoxLayout, QFileDialog
 
-
+#first window creation
 app = QtWidgets.QApplication(sys.argv)
 Dialog = QtWidgets.QDialog() 
 ui = Ui_Plotter()
 
+#second window variable define
+ui2 = Ui_MainWindow()
+
 #importing csv file 
 def open_file(self):
-        try:
+        # try:
             path = QFileDialog.getOpenFileName(filter='*.csv')          
             if path != ('', ''):
                 print(path[0])
-                ui.pushButton.clicked.connect(ui.open_sec_win)
+
+                #proceed button
+                ui.proceed.clicked.connect(ui.open_sec_win)
+                
                 ui.label_5.setText("File imported successfully")
-        except: 
-            ui.label_5.setText("File not imported")
+        # except: 
+        #     ui.label_5.setText("File not imported")
 
 if __name__ == "__main__":
     
@@ -27,6 +33,6 @@ if __name__ == "__main__":
     
     #import button
     ui.importing.clicked.connect(open_file)
-    
+
     Dialog.show()
     sys.exit(app.exec_())
